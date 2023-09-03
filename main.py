bbda32d6a7877bdc448e2d33f90f249cb53af07a
Edwin Wang <edwin-wang@users.noreply.github.com>
2023-09-03 14:03:50 +0800

    Use javascript instead of python

--- main.py
+++ main.py
@@ -3,73 +3,68 @@ elapsed_time = 0
 start_time = 0
 timer_started = False
 music_played = False
-detect_time = 0
-min_time = 0
-sec_time = 0
-min_str = ""
-sec_str = ""
-show_time = ""
 # 检测时间，单位：分钟
 detect_time = 1
 # 显示开机画面
-basic.show_icon(IconNames.HAPPY)
 basic.show_icon(IconNames.HEART)
-basic.clear_screen()
-pause(500)
+basic.pause(800)
 basic.show_string("GO!")
 music.set_volume(0)
 music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
     music.PlaybackMode.LOOPING_IN_BACKGROUND)
 
-
 def on_forever():
     global music_played, timer_started, start_time, elapsed_time
     if input.pin_is_pressed(TouchPin.P2):
         # 按钮被按下亮绿灯，并且重置变量和设置喇叭静音
-        pins.digital_write_pin(DigitalPin.P8, 1)
-        pins.digital_write_pin(DigitalPin.P12, 0)
+        on_green(True)
         music_played = False
         timer_started = False
         music.set_volume(0)
-        led.enable(False)
-        pause(3200)
         start_time = 0
         elapsed_time = 0
     else:
         # 按钮释放亮红灯
-        pins.digital_write_pin(DigitalPin.P8, 0)
-        pins.digital_write_pin(DigitalPin.P12, 1)
+        on_green(False)
         # 若计时器没有开始计时，获取计时器开始计时的时间并且置位计时器变量
-        if not (timer_started):
+        if not timer_started:
             start_time = input.running_time()
             timer_started = True
-            led.enable(True)
         else:
             # 计算从开始计时至今的逝去时间，单位为毫秒
             elapsed_time = input.running_time() - start_time
             # 当逝去时间超过检测时间，开始报警
             if elapsed_time >= detect_time * 20 * 1000:
-                if not (music_played):
+                if not music_played:
                     music_played = True
                     music.set_volume(255)
 basic.forever(on_forever)
 
 # 时间显示
+
 def on_forever2():
-    global elapsed_seconds
     if timer_started == True:
-        # 处理分钟和秒数的时间显示格式
-        elapsed_seconds = Math.idiv(elapsed_time, 1000)
-        min_time = Math.idiv(elapsed_seconds, 60)
-        sec_time = elapsed_seconds % 60
-        if min_time < 10:
-            min_str = "0" + ("" + str(min_time))
-        else:
-            min_str = "" + str(min_time)
-        if sec_time < 10:
-            sec_str = "0" + ("" + str(sec_time))
-        else:
-            sec_str = "" + str(sec_time)
-        show_time = "" + min_str + ":" + ("" + sec_str)
-        basic.show_string(show_time, 90)
+        basic.show_string(show_time(elapsed_time), 80)
 basic.forever(on_forever2)
+
+# 处理分钟和秒数的时间显示格式
+def show_time(sec: number):
+    elapsed_seconds2 = Math.idiv(sec, 1000)
+    min_time = Math.idiv(elapsed_seconds2, 60)
+    sec_time = elapsed_seconds2 % 60
+    if min_time < 10:
+        min_str = "0" + ("" + str(min_time))
+    else:
+        min_str = "" + ("" + str(min_time))
+    if sec_time < 10:
+        sec_str = "0" + ("" + str(sec_time))
+    else:
+        sec_str = "" + ("" + str(sec_time))
+    return str(min_str) + ":" + str(sec_str)
+def on_green(yes: bool):
+    if yes:
+        pins.digital_write_pin(DigitalPin.P8, 1)
+        pins.digital_write_pin(DigitalPin.P12, 0)
+    else:
+        pins.digital_write_pin(DigitalPin.P8, 0)
+        pins.digital_write_pin(DigitalPin.P12, 1)
--- main.ts
+++ main.ts
@@ -3,19 +3,11 @@ let elapsed_time = 0
 let start_time = 0
 let timer_started = false
 let music_played = false
-let detect_time = 0
-let min_time = 0
-let sec_time = 0
-let min_str = ""
-let sec_str = ""
-let show_time = ""
 //  检测时间，单位：分钟
-detect_time = 1
+let detect_time = 1
 //  显示开机画面
-basic.showIcon(IconNames.Happy)
 basic.showIcon(IconNames.Heart)
-basic.clearScreen()
-pause(500)
+basic.pause(800)
 basic.showString("GO!")
 music.setVolume(0)
 music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.LoopingInBackground)
@@ -23,24 +15,19 @@ basic.forever(function on_forever() {
     
     if (input.pinIsPressed(TouchPin.P2)) {
         //  按钮被按下亮绿灯，并且重置变量和设置喇叭静音
-        pins.digitalWritePin(DigitalPin.P8, 1)
-        pins.digitalWritePin(DigitalPin.P12, 0)
+        on_green(true)
         music_played = false
         timer_started = false
         music.setVolume(0)
-        led.enable(false)
-        pause(3200)
         start_time = 0
         elapsed_time = 0
     } else {
         //  按钮释放亮红灯
-        pins.digitalWritePin(DigitalPin.P8, 0)
-        pins.digitalWritePin(DigitalPin.P12, 1)
+        on_green(false)
         //  若计时器没有开始计时，获取计时器开始计时的时间并且置位计时器变量
         if (!timer_started) {
             start_time = input.runningTime()
             timer_started = true
-            led.enable(true)
         } else {
             //  计算从开始计时至今的逝去时间，单位为毫秒
             elapsed_time = input.runningTime() - start_time
@@ -60,31 +47,41 @@ basic.forever(function on_forever() {
 })
 //  时间显示
 basic.forever(function on_forever2() {
-    let min_time: number;
-    let sec_time: number;
-    let min_str: string;
-    let sec_str: string;
-    let show_time: string;
-    
     if (timer_started == true) {
-        //  处理分钟和秒数的时间显示格式
-        elapsed_seconds = Math.idiv(elapsed_time, 1000)
-        min_time = Math.idiv(elapsed_seconds, 60)
-        sec_time = elapsed_seconds % 60
-        if (min_time < 10) {
-            min_str = "0" + ("" + ("" + min_time))
-        } else {
-            min_str = "" + ("" + min_time)
-        }
-        
-        if (sec_time < 10) {
-            sec_str = "0" + ("" + ("" + sec_time))
-        } else {
-            sec_str = "" + ("" + sec_time)
-        }
-        
-        show_time = "" + min_str + ":" + ("" + sec_str)
-        basic.showString(show_time, 90)
+        basic.showString(show_time(elapsed_time), 80)
     }
     
 })
+//  处理分钟和秒数的时间显示格式
+function show_time(sec: number): string {
+    let min_str: any;
+    let sec_str: any;
+    let elapsed_seconds2 = Math.idiv(sec, 1000)
+    let min_time = Math.idiv(elapsed_seconds2, 60)
+    let sec_time = elapsed_seconds2 % 60
+    if (min_time < 10) {
+        min_str = "0" + ("" + ("" + min_time))
+    } else {
+        min_str = "" + ("" + ("" + min_time))
+    }
+    
+    if (sec_time < 10) {
+        sec_str = "0" + ("" + ("" + sec_time))
+    } else {
+        sec_str = "" + ("" + ("" + sec_time))
+    }
+    
+    return "" + min_str + ":" + ("" + sec_str)
+}
+
+function on_green(yes: boolean) {
+    if (yes) {
+        pins.digitalWritePin(DigitalPin.P8, 1)
+        pins.digitalWritePin(DigitalPin.P12, 0)
+    } else {
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        pins.digitalWritePin(DigitalPin.P12, 1)
+    }
+    
+}
+
